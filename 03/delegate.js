b1c3e4bbefa95dd48b722d4467949a2645272f1a
Tűri Erik <54734700+turierik@users.noreply.github.com>
2023-10-03 08:07:50 +0200

    Create delegate.js

---
+++ 03/delegate.js
@@ -0,0 +1,6 @@
+function delegate(parent, type, selector, handler) {
+    parent.addEventListener(type, function (event) {
+        const targetElement = event.target.closest(selector)
+        if (this.contains(targetElement)) handler.call(targetElement, event)
+    })
+}
